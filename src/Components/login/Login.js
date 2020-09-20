448fccc5fa02aa5b73feb217c5607a51fbc9a452
duruchima <duruchima1@gmail.com>
2020-09-20 15:24:04 -0400

    cleaned up login component

--- src/Components/login/Login.js
+++ src/Components/login/Login.js
@@ -4,12 +4,9 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
@@ -46,10 +43,6 @@ const useStyles = makeStyles((theme) => ({
         margin: theme.spacing(3, 0, 2),
     },
 }));
-
-function handleClick() {
-    console.log("click event worked")
-}
 
 export default function LogIn(props) {
     const classes = useStyles();
@@ -89,10 +82,6 @@ export default function LogIn(props) {
                         autoComplete="current-password"
                         onChange={props.handleChange}
                     />
-                    {/* <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
-                    /> */}
                     <Button
                         type="submit"
                         fullWidth
