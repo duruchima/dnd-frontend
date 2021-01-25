5872720d727837e38323724a72cf5d8abfd4775e
duruchima <duruchima1@gmail.com>
2021-01-24 19:24:04 -0500

    modified warlock

--- src/Components/character/CharacterCreateForm.js
+++ src/Components/character/CharacterCreateForm.js
@@ -1284,6 +1284,14 @@ class CharacterCreateForm extends React.Component {
                                 <Checkbox checked={this.state.charSkills.indexOf("Spiderclimb") > -1} />
                                 <ListItemText primary={"Spiderclimb"} />
                             </MenuItem>
+                            <MenuItem key={"Suggestion"} value={"Suggestion"}>
+                                <Checkbox checked={this.state.charSkills.indexOf("Suggestion") > -1} />
+                                <ListItemText primary={"Suggestion"} />
+                            </MenuItem>
+                            <MenuItem key={"Counterspell"} value={"Counterspell"}>
+                                <Checkbox checked={this.state.charSkills.indexOf("Counterspell") > -1} />
+                                <ListItemText primary={"Counterspell"} />
+                            </MenuItem>
                         </Select>
                         </FormControl>
                     </> : null}
